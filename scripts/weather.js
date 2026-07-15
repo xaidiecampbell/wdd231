0e5b2f8b4e728279464e34b248c0126c164ce579
XaidieCampbell <xcampbell@>
2026-07-15 10:20:04 -0400

    try1

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,17 +1,25 @@
-alert("Javascript file is working")
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
-const url = const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.77&lon=6.64&units=imperial&appid=23858379a0a8586a573c18f553493fee";
-// const response = url
+// alert("Javascript file is working")
+// const currentTemp = document.querySelector('#current-temp');
+// const weatherIcon = document.querySelector('#weather-icon');
+// const captionDesc = document.querySelector('figcaption');
+const myTown = document.querySelector('#town');
+const myDescription = document.querySelector('#description');
+const myTemperature = document.querySelector('#temperature');
+const myGraphic = document.querySelector('#graphic');
+const myKey = "23858379a0a8586a573c18f553493fee"
+const myLat = "49.77"
+const myLon = "6.64"
+const myUrl = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLon}&appid=${myKey}&units=imperial`;
+
+
 
 async function apiFetch() {
     try {
-        const response = await fetch(url);
+        const response = await fetch(myUrl);
         if (response.ok) {
             const data = await response.json();
             console.log(data); // testing only
-            // displayResults(data); // uncomment when ready
+            displayResults(data); // uncomment when ready
         } else {
             throw Error(await response.text());
         }
@@ -21,3 +29,15 @@ async function apiFetch() {
 }
 
 apiFetch();
+
+function displayResults(data) {
+    console.log('hello')
+    myTown.innerHTLM = data.name
+    myDescription.innerHTML = data.weather[0].description
+    myTemperature.innerHTML = `${data.main.temp}&deg;F`;
+    const iconSrc = `https://openweathermap.org/payload/api/media/file/${data.weather[0].icon}@2x.png`;
+    let desc = data.weather[0].______;
+    myGraphic.setAttribute('src', iconSrc);
+    myGraphic.setAttribute('alt', data.weather[0].description);
+    
+}
